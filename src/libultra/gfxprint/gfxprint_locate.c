0be91c36f73e0cb8e3b5968f2e86fd79fcc7f655
Cuyler36 <jeremy96@vt.edu>
2023-02-22 03:28:37 -0500

    Match all entries in gfxprint

--- src/libultra/gfxprint/gfxprint_locate.c
+++
@@ -1,6 +0,0 @@
-#include "libultra/gfxprint.h"
-
-void gfxprint_locate(struct gfxprint_obj* gfxprint, s32 x, s32 y) {
-    gfxprint->NewX = (s16) (gfxprint->PrevX + (x * 4));
-    gfxprint->NewY = (s16) (gfxprint->PrevY + (y * 4));
-}
