4e1bd110d38538467ffa8a324d64969f4360c511
Cuyler36 <jeremy96@vt.edu>
2023-02-22 17:17:46 -0500

    Use func pointer directly instead of referencing member

--- src/libc64/aprintf.c
+++ src/libc64/aprintf.c
@@ -2,5 +2,5 @@
 #include "libultra/xprintf.h" /* _Printf */
 
 extern int vaprintf(aprout_func_t* func, const char* fmt, va_list ap) {
-  return _Printf(func->prout_func, &func->prout_func, fmt, ap);
+  return _Printf(func->prout_func, func, fmt, ap);
 }
