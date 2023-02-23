5f5ff2cb2fd4e9dffdf1ec586a762465ee7a9d8a
Cuyler36 <jeremy96@vt.edu>
2023-02-22 22:29:35 -0500

    Implement JSystem/JKernel/JKRDisposer.cpp

---
+++ src/JSystem/JKernel/JKRDisposer.cpp
@@ -0,0 +1,15 @@
+#include "JSystem/JKernel/JKRDisposer.h"
+#include "JSystem/JKernel/JKRHeap.h"
+
+JKRDisposer::JKRDisposer() : mPointerLinks(this) {
+  this->mRootHeap = JKRHeap::findFromRoot(this);
+  if (this->mRootHeap != nullptr) {
+    this->mRootHeap->appendDisposer(this);
+  }
+}
+
+JKRDisposer::~JKRDisposer() {
+  if (this->mRootHeap != nullptr) {
+    this->mRootHeap->removeDisposer(this);
+  }
+}
