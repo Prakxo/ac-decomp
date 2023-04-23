98d13299cad2f6f4b9536ed79479ff55ecd824b7
Prakxo <87568477+Prakxo@users.noreply.github.com>
2023-04-23 20:15:03 +0200

    Fix building

--- include/m_flashrom.h
+++ include/m_flashrom.h
@@ -10,20 +10,6 @@ extern "C" {
 
 #define mFRm_SAVE_ID ('GAFE')
 
-extern void mFRm_set_msg_idx(int idx);
-extern int mFRm_get_msg_idx();
-extern u16 mFRm_ReturnCheckSum(u16* data, s32 size);
-extern u16 mFRm_GetFlatCheckSum(u16* data, s32 size, s16 now_checksum);
-extern int mFRm_CheckSaveData_ID(mFRm_chk_t* save_check);
-extern int mFRm_CheckSaveData_common(mFRm_chk_t* save_check, u16 land_id);
-extern int mFRm_CheckSaveData();
-extern void mFRm_ClearSaveCheckData(mFRm_chk_t* save_check);
-extern void mFRm_SetSaveCheckData(mFRm_chk_t* save_check);
-extern void mFRm_PrintSavedDebug(gfxprint_t* gfxprint);
-extern void mFRm_clear_err_info();
-extern void mFRm_save_data_check();
-extern void mFRm_display_errInfo(gfxprint_t* gfxprint);
-
 #define mFRm_ERROR_INFO_NUM 10
 #define mFRm_NO_ERROR_NO 7
 
@@ -42,6 +28,20 @@ typedef struct flashrom_error_info {
 
 typedef int (save_check_proc)(void);
 
+extern void mFRm_set_msg_idx(int idx);
+extern int mFRm_get_msg_idx();
+extern u16 mFRm_ReturnCheckSum(u16* data, s32 size);
+extern u16 mFRm_GetFlatCheckSum(u16* data, s32 size, s16 now_checksum);
+extern int mFRm_CheckSaveData_ID(mFRm_chk_t* save_check);
+extern int mFRm_CheckSaveData_common(mFRm_chk_t* save_check, u16 land_id);
+extern int mFRm_CheckSaveData();
+extern void mFRm_ClearSaveCheckData(mFRm_chk_t* save_check);
+extern void mFRm_SetSaveCheckData(mFRm_chk_t* save_check);
+extern void mFRm_PrintSavedDebug(gfxprint_t* gfxprint);
+extern void mFRm_clear_err_info();
+extern void mFRm_save_data_check();
+extern void mFRm_display_errInfo(gfxprint_t* gfxprint);  
+  
 #ifdef __cplusplus
 }
 #endif
