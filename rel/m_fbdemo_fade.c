ba149cdb62ccc359451f9d5d8cde58825145bec5
Prakxo <lycos2003@gmail.com>
2023-05-12 12:00:09 +0000

    link m_fbdemo_fade

--- rel/m_fbdemo_fade.c
+++ rel/m_fbdemo_fade.c
@@ -5,12 +5,15 @@
 
 
 Gfx fbdemo_fade_gfx_init[] = {
-
     gsDPPipeSync(),
     gsSPClearGeometryMode(G_ZBUFFER | G_SHADE | G_CULL_BOTH | G_FOG | G_LIGHTING | G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR | G_LOD | G_SHADING_SMOOTH),
-    gsDPSetOtherMode(G_AD_DISABLE | G_CD_MAGICSQ | G_CK_NONE | G_TC_FILT | G_TF_BILERP | G_TT_NONE | G_TL_TILE | G_TD_CLAMP | G_TP_NONE | G_CYC_1CYCLE | G_PM_1PRIMITIVE, G_AC_DITHER | G_ZS_PRIM | AA_EN | Z_CMP | Z_UPD | IM_RD | CLR_ON_CVG | CVG_DST_SAVE | ZMODE_DEC | CVG_X_ALPHA | ALPHA_CVG_SEL | FORCE_BL | GBL_c1(G_BL_CLR_FOG, G_BL_0, G_BL_CLR_FOG, G_BL_0) | GBL_c2(G_BL_CLR_FOG, G_BL_A_IN, G_BL_CLR_FOG, G_BL_0)),
-    gsDPSetColorImage(7, G_IM_SIZ_32b, 1788, 0xDF000000),
+    gsDPSetOtherMode(G_AD_DISABLE | G_CD_MAGICSQ | G_CK_NONE | G_TC_FILT | G_TF_BILERP | G_TT_NONE | G_TL_TILE | G_TD_CLAMP | G_TP_NONE | G_CYC_1CYCLE | G_PM_1PRIMITIVE, G_AC_NONE | G_ZS_PIXEL | G_RM_CLD_SURF | G_RM_CLD_SURF2),
+    gsDPSetCombineMode(G_CC_PRIMITIVE, G_CC_PRIMITIVE),
+    gsSPEndDisplayList(),
 };
+
+
+
 
 
 TransitionFade* fbdemo_fade_init(TransitionFade* this){
@@ -64,15 +67,17 @@ void fbdemo_fade_draw(TransitionFade* this, Gfx** gfxP){
 }
 
 void fbdemo_fade_startup(TransitionFade* this){
+    static s8 start_frame[] = {0x00, 0xFA, 0x00, 0x00, 
+                               0x00, 0x00, 0xAB, 0x00, 
+                               0x00, 0x00, 0x00, 0x00, 
+                               0x00, 0x00, 0x00, 0x00};
+    
+    
     static u8 start_color[] = {0x00, 0xFF, 0x00, 0x00, 
                                0x00, 0x00, 0x00, 0x00, 
                                0x00, 0x00, 0x00, 0x00, 
                                0x00, 0x00, 0x00, 0x00};
                                
-    static s8 start_frame[] = {0x00, 0xFA, 0x00, 0x00, 
-                               0x00, 0x00, 0xAB, 0x00, 
-                               0x00, 0x00, 0x00, 0x00, 
-                               0x00, 0x00, 0x00, 0x00};
     
 
     this->color.a = start_color[this->type];
@@ -94,7 +99,7 @@ void fbdemo_fade_setcolor_rgba8888(TransitionFade* this, u32 color){
     this->color.rgba = color;
 }
 
-int fbdemo_fade_is_finish(TransitionFade* this){
+u8 fbdemo_fade_is_finish(TransitionFade* this){
     
     return this->isDone;
 }
