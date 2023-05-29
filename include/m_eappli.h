712c6be4800ea6c5325242db22ab422783a2baba
Cuyler36 <jeremy96@vt.edu>
2023-05-29 02:58:12 -0400

    Add header declarations

--- include/m_eappli.h
+++ include/m_eappli.h
@@ -7,7 +7,13 @@
 extern "C" {
 #endif
 
-
+extern void mEA_InitLetterCardE();
+extern int mEA_CheckLetterCardE(int card_no);
+extern void mEA_SetLetterCardE(int card_no);
+extern void mEA_GetCardDLProgram();
+extern void mEA_CleanCardDLProgram();
+extern u8* mEA_dl_carde_program_p();
+extern size_t mEA_dl_carde_program_size();
 
 #ifdef __cplusplus
 }
