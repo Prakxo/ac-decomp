c75716e58c7485239005f9fcf0572e9c80f26c5b
Cuyler36 <jeremy96@vt.edu>
2023-08-29 16:03:25 -0400

    Implement & link m_calendar.c

--- include/m_calendar.h
+++ include/m_calendar.h
@@ -8,6 +8,14 @@
 extern "C" {
 #endif
 
+#define mCD_FLAG_MOTHERS_DAY (1 << 0)
+#define mCD_FLAG_TOWN_DAY (1 << 1)
+#define mCD_FLAG_METEOR_SHOWER (1 << 2)
+#define mCD_FLAG_FOUNDERS_DAY (1 << 3)
+#define mCD_FLAG_HARVEST_MOON_9 (1 << 4)
+#define mCD_FLAG_HARVEST_MOON_10 (1 << 5)
+#define mCD_FLAG_OFFICERS_DAY (1 << 6)
+
 /* sizeof(mCD_player_calendar_c) == 0x68 */
 typedef struct calendar_player_info_s {
   /* 0x00 */ u32 played_days[lbRTC_MONTHS_MAX]; /* bitfield of days where 1 bit represents a day played */
@@ -17,6 +25,12 @@ typedef struct calendar_player_info_s {
   /* 0x64 */ lbRTC_year_t year; /* year calendar was last updated */
   /* 0x66 */ lbRTC_month_t month; /* month calendar was last updated */
 } mCD_player_calendar_c;
+
+extern void mCD_calendar_clear(int player_no);
+extern void mCD_calendar_check_delete(int player_no, lbRTC_year_t year, lbRTC_month_t month, lbRTC_day_t day);
+extern void mCD_calendar_wellcome_on();
+extern void mCD_calendar_event_on(lbRTC_year_t year, lbRTC_month_t month, lbRTC_day_t day, u8 event);
+extern int mCD_calendar_event_check(lbRTC_year_t year, lbRTC_month_t month, lbRTC_day_t day, int player_no, u8 event);
 
 #ifdef __cplusplus
 }
