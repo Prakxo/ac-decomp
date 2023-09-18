e9f9d94cc48bb95f5f3d0473d18437d00d59df54
Prakxo <lycos2003@gmail.com>
2023-09-18 20:38:31 +0200

    link ac_cobra

--- include/ac_t_cobra1.h
+++ include/ac_t_cobra1.h
@@ -3,12 +3,24 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_tools.h"
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 extern ACTOR_PROFILE T_Cobra1_Profile;
+
+typedef void (*COBRA1_PROC)(ACTOR*);
+
+typedef struct t_cobra1_s{
+    TOOLS_ACTOR tools_class;
+    u8 pad2[0x8];
+    COBRA1_PROC proc; 
+    int current_id;
+}COBRA1_ACTOR;
+
 
 #ifdef __cplusplus
 }
---
+++ rel/ac_t_cobra1.c
@@ -0,0 +1,90 @@
+#include "ac_t_cobra1.h"
+
+#include "m_name_table.h"
+#include "sys_matrix.h"
+#include "m_lib.h"
+#include "m_rcp.h"
+
+
+static void aTCB1_actor_ct(ACTOR* actor, GAME*);
+static void aTCB1_actor_move(ACTOR* actor, GAME*);
+static void aTCB1_actor_draw(ACTOR* actor, GAME*);
+static void aTCB1_setupAction(ACTOR*, int);
+
+ACTOR_PROFILE T_Cobra1_Profile = {
+    mAc_PROFILE_T_COBRA1,
+    ACTOR_PART_BG,
+    ACTOR_STATE_NO_DRAW_WHILE_CULLED | ACTOR_STATE_NO_MOVE_WHILE_CULLED,
+    EMPTY_NO,
+    ACTOR_OBJ_BANK_16,
+    sizeof(COBRA1_ACTOR),
+    &aTCB1_actor_ct,
+    NONE_ACTOR_PROC,
+    &aTCB1_actor_move,
+    &aTCB1_actor_draw,
+    NULL
+};
+
+extern Gfx crw_cobra_model[];
+
+static void aTCB1_actor_ct(ACTOR* actor, GAME*){
+
+    aTCB1_setupAction(actor,4);
+}
+
+static void aTCB1_destruct(ACTOR* actor){
+
+    Actor_delete(actor);
+}
+
+
+static void aTCB1_setupAction(ACTOR*actor, int action){
+    COBRA1_ACTOR* biscus = (COBRA1_ACTOR*)actor;
+    static COBRA1_PROC process[] = {
+        (COBRA1_PROC)none_proc1,(COBRA1_PROC)none_proc1,(COBRA1_PROC)none_proc1,aTCB1_destruct,(COBRA1_PROC)none_proc1,NULL
+    };
+
+    biscus->proc = process[action];
+    biscus->current_id = action;
+    biscus->tools_class.process_id = action;
+
+}
+
+static void aTCB1_actor_move(ACTOR* actor, GAME*){
+    COBRA1_ACTOR* biscus = (COBRA1_ACTOR*)actor;
+
+    if(biscus->tools_class.process_id != biscus->current_id){
+        aTCB1_setupAction(actor, biscus->tools_class.process_id);
+    }
+
+    biscus->proc(actor);
+}
+
+static void aTCB1_actor_draw(ACTOR* actor, GAME* game){
+    COBRA1_ACTOR* biscus = (COBRA1_ACTOR*)actor;
+
+    GRAPH* graph;
+    Gfx* gfxp;
+
+    if(biscus->tools_class.enable == 1){
+        graph = game->graph;
+        
+        OPEN_DISP(graph);
+        
+        Matrix_put(&biscus->tools_class.matrix_work);
+        Matrix_Position_Zero(&biscus->tools_class.actor_class.world.position);
+        
+        biscus->tools_class.enable = 0;
+    
+        _texture_z_light_fog_prim_npc(graph); 
+
+        gfxp = NOW_POLY_OPA_DISP;
+        gSPMatrix(gfxp++, _Matrix_to_Mtx_new(graph),G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPDisplayList(gfxp++, crw_cobra_model);
+        SET_POLY_OPA_DISP(gfxp);
+        
+        CLOSE_DISP(graph);
+    }
+
+        
+}
