bc4b6ee51230c5009a74d24941953fd41da5e2b0
Prakxo <lycos2003@gmail.com>
2023-10-09 16:41:12 +0200

    link ef_room_sunshine_minsect

--- include/ef_room_sunshine_minsect.h
+++ include/ef_room_sunshine_minsect.h
@@ -3,12 +3,19 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ef_room_sunshine.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 extern ACTOR_PROFILE Room_Sunshine_Minsect_Profile;
+
+typedef struct effect_room_sunshine_minsect_s{
+    ROOMSUNSHINE_ACTOR ef_sunshine_class;
+}ROOMSUNSHINEMINSECT_ACTOR;   
+
+extern u8 draw_status;
 
 #ifdef __cplusplus
 }
