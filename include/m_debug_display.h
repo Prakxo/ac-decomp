5e7e178c212d43412f187949d0db77a1ed7152cf
Prakxo <lycos2003@gmail.com>
2023-10-21 14:57:33 +0200

    link debug display

--- include/m_debug_display.h
+++ include/m_debug_display.h
@@ -4,8 +4,23 @@
 #include "types.h"
 #include "m_play.h"
 
+typedef struct debug_display_s Debug_display;
 
-extern void Debug_Display_output(GAME_PLAY*);
+struct debug_display_s{
+    xyz_t pos;
+    s_xyz rot;
+    xyz_t scale;
+    rgba8888_t color;
+    s16 type;
+    Debug_display* next;
+};
 extern void Debug_Display_init();
+extern Debug_display* Debug_Display_new(f32 posX, f32 posY, f32 posZ, f32 scaleX, f32 scaleY, f32 scaleZ,
+s16 rotX, s16 rotY, s16 rotZ, s8 r, s8 g, s8 b, s8 alpha, s16 type, GRAPH* graph);
+extern void Debug_Display_output(GAME_PLAY* play);
+
+
+
+extern Debug_display* debug_display;
 
 #endif
