97d1b303ae5c332a2ffc9ba23ac1b26482440e17
Cuyler36 <jeremy96@vt.edu>
2023-11-25 12:59:08 -0500

    Link libc64/math64.c

--- src/libc64/math64.c
+++ src/libc64/math64.c
@@ -1,12 +1,14 @@
 #include "libc/math.h"
 #include "MSL_C/w_math.h"
-f32 fatan2(f32 x, f32 y){
+
+f32 fatan2(f32 x, f32 y) {
     return atan2(x, y);
 }
-f64 fsqrt(f32 x){
+
+f64 fsqrt(f32 x) {
     return sqrtf(x);
 }
 
-f32 facos(f32 x){
+f32 facos(f32 x) {
     return acos(x);
 }
