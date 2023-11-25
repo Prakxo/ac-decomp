b0bd815982f70c7b144786a1dcf166ff9f7dff06
Cuyler36 <jeremy96@vt.edu>
2023-11-25 12:32:54 -0500

    Link libu64/debug.c

--- src/libu64/debug.c
+++ src/libu64/debug.c
@@ -1,8 +1,9 @@
 #include "libu64/debug.h"
 #include "libultra/osThread.h"
+#include "libc64/sprintf.h"
 
-void _dbg_hungup(const char* name, s32 line) {
-char dbg_tbuf[0x100];
-
-sprintf(dbg_tbuf, "*** HungUp in thread %d, [%s:%d] ***\n",osGetThreadId(0), name, line);
+extern void _dbg_hungup(const char* file, int line) {
+  char dbg_tbuf[0x100];
+    
+  sprintf(dbg_tbuf, "*** HungUp in thread %d, [%s:%d] ***\n", osGetThreadId(0), file, line);
 }
