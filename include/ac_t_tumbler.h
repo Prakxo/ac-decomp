6f1c8931b2faaae1d77ac7e18ec63574d0bceae7
Cuyler36 <jeremy96@vt.edu>
2023-12-07 18:21:42 -0500

    Implement & link ac_t_tumbler

--- include/ac_t_tumbler.h
+++ include/ac_t_tumbler.h
@@ -3,10 +3,24 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_tools.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct tool_tumbler_s TOOL_TUMBLER_ACTOR;
+typedef void (*aTTB_ACTION_PROC)(TOOL_TUMBLER_ACTOR*);
+
+struct tool_tumbler_s {
+  TOOLS_ACTOR tools_class;
+  aTTB_ACTION_PROC action_proc;
+  int action;
+};
+
+extern void aTTB_actor_ct(ACTOR* actorx, GAME* game);
+extern void aTTB_actor_move(ACTOR* actorx, GAME* game);
+extern void aTTB_actor_draw(ACTOR* actorx, GAME* game);
 
 extern ACTOR_PROFILE T_Tumbler_Profile;
 
