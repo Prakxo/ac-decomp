c6d06b3f3ebf11639e7c9bcdeb91fe4aee484efb
Cuyler36 <jeremy96@vt.edu>
2023-12-10 21:25:34 -0500

    Implement & link m_notice_ovl

---
+++ include/m_notice_ovl_h.h
@@ -0,0 +1,16 @@
+#ifndef M_NOTICE_OVL_H_H
+#define M_NOTICE_OVL_H_H
+
+#include "types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct notice_ovl_s mNT_Ovl_c;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
