5a73b944d15323b7892f5c693340fe7715b50c21
Cuyler36 <jeremy96@vt.edu>
2023-12-19 07:58:39 -0500

    Implement & link ac_ev_gypsy

--- include/ac_ev_gypsy.h
+++ include/ac_ev_gypsy.h
@@ -3,10 +3,27 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_npc.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define aEGPS_FORTUNE_PRICE 50
+
+typedef struct ev_gypsy_actor_s EV_GYPSY_ACTOR;
+
+typedef void (*aEGPS_TALK_INIT_PROC)(EV_GYPSY_ACTOR*, GAME_PLAY*);
+typedef void (*aEGPS_TALK_PROC)(EV_GYPSY_ACTOR*, GAME_PLAY*);
+typedef void (*aEGPS_TALK_SETUP_PROC)(EV_GYPSY_ACTOR*, GAME_PLAY*, int);
+
+struct ev_gypsy_actor_s {
+  NPC_ACTOR npc_class;
+  int talk_action;
+  aEGPS_TALK_PROC talk_proc;
+  aEGPS_TALK_SETUP_PROC setup_talk_proc;
+  int fortune_given;
+};
 
 extern ACTOR_PROFILE Ev_Gypsy_Profile;
 
