50b13bea9d5def99524fd260c0141f6c64b43b16
Prakxo <lycos2003@gmail.com>
2024-01-10 17:43:39 +0100

    link kago

--- include/ac_kago.h
+++ include/ac_kago.h
@@ -3,10 +3,16 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_structure.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct actor_kago_s{
+    STRUCTURE_ACTOR structure_class;
+}KAGO_ACTOR;
+
 
 extern ACTOR_PROFILE Kago_Profile;
 
