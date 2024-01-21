899cae3c9b4134dd313a3615413636b8f034dfba
Cuyler36 <jeremy96@vt.edu>
2024-01-21 15:29:14 -0500

    Implement & link ac_ev_kabuPeddler

--- include/ac_ev_kabuPeddler.h
+++ include/ac_ev_kabuPeddler.h
@@ -3,10 +3,26 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_npc.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define aEKPD_KABU_TYPE_NUM 4
+
+typedef struct ev_kabuPeddler_s EV_KABUPEDDLER_ACTOR;
+
+typedef void (*aEKPD_PROC)(EV_KABUPEDDLER_ACTOR*, GAME_PLAY*);
+typedef void (*aEKPD_SETUP_PROC)(EV_KABUPEDDLER_ACTOR*, GAME_PLAY*, int);
+
+struct ev_kabuPeddler_s {
+    NPC_ACTOR npc_class;
+    int kabu_type;
+    int talk_act;
+    aEKPD_PROC talk_proc;
+    aEKPD_SETUP_PROC setup_talk_proc;
+};
 
 extern ACTOR_PROFILE Ev_KabuPeddler_Profile;
 
