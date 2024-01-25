99f9820e491a832198ba8dce342ed6304354cc19
Hexalotl <15166449+Hexalotl@users.noreply.github.com>
2024-01-25 08:57:43 -0800

    Implement and link ac_t_tama with major help from Cuyler and Prakxo (#235)

--- include/ac_t_tama.h
+++ include/ac_t_tama.h
@@ -3,12 +3,17 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_tools.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 extern ACTOR_PROFILE T_Tama_Profile;
+
+typedef struct t_tama_s {
+    TOOLS_ACTOR tools_class;
+} T_TAMA_ACTOR;
 
 #ifdef __cplusplus
 }
