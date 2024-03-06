07f4a3ee98b92653352cba2be9cde84e6e54a520
Cuyler36 <jeremy96@vt.edu>
2024-03-06 06:01:46 -0500

    Finish more furniture actors

--- src/ftr/ac_tool.c
+++ src/ftr/ac_tool.c
@@ -1,17 +1,68 @@
-static void fTOOL_ct(FTR_ACTOR* ftr_actor, u8* data);
-static void fTOOL_mv(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data);
-static void fTOOL_dw(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data);
-static void fTOOL_dt(FTR_ACTOR* ftr_actor, u8* data);
+static void fTOOL_ct(FTR_ACTOR* ftr_actor, u8* data) {
+    // nothing
+}
+
+static void fTOOL_mv(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data) {
+    // nothing
+}
+
+static void fTOOL_dt(FTR_ACTOR* ftr_actor, u8* data) {
+    // nothing
+}
+
+extern Gfx obj_netT_gfx_model[];
+extern Gfx obj_netT_mat_model[];
+
+extern Gfx obj_axeT_gfx_model[];
+extern Gfx obj_axeT_mat_model[];
+
+extern Gfx obj_shovelT_gfx_model[];
+extern Gfx obj_shovelT_mat_model[];
+
+extern Gfx obj_rodT_gfx_model[];
+extern Gfx obj_rodT_mat_model[];
+
+extern Gfx obj_rod_lineT_gfx_model[];
+extern Gfx obj_rod_lineT_mat_model[];
+
+static Gfx* fTOOL_mat_table[] = {
+    obj_netT_mat_model,
+    obj_axeT_mat_model,
+    obj_shovelT_mat_model,
+    obj_rod_lineT_mat_model,
+};
+
+static Gfx* fTOOL_gfx_table[] = {
+    obj_netT_gfx_model,
+    obj_axeT_gfx_model,
+    obj_shovelT_gfx_model,
+    obj_rod_lineT_gfx_model,
+};
+
+static void fTOOL_dw(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data) {
+    int tool_idx = (ftr_actor->name - 0x463) & 3;
+
+    OPEN_DISP(game->graph);
+
+    gSPMatrix(NEXT_POLY_OPA_DISP, _Matrix_to_Mtx_new(game->graph), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPDisplayList(NEXT_POLY_OPA_DISP, fTOOL_mat_table[tool_idx]);
+    gSPDisplayList(NEXT_POLY_OPA_DISP, fTOOL_gfx_table[tool_idx]);
+
+    if (ftr_actor->name == 0x466) {
+        gSPMatrix(NEXT_POLY_XLU_DISP, _Matrix_to_Mtx_new(game->graph), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPDisplayList(NEXT_POLY_XLU_DISP, obj_rodT_mat_model);
+        gSPDisplayList(NEXT_POLY_XLU_DISP, obj_rodT_gfx_model);
+    }
+
+    CLOSE_DISP(game->graph);
+}
 
 static aFTR_vtable_c fTOOL_func = {
-	&fTOOL_ct,
-	&fTOOL_mv,
-	&fTOOL_dw,
-	&fTOOL_dt,
-	NULL,
+    &fTOOL_ct, &fTOOL_mv, &fTOOL_dw, &fTOOL_dt, NULL,
 };
 
 aFTR_PROFILE iam_tool = {
+    // clang-format off
 	NULL,
 	NULL,
 	NULL,
@@ -29,4 +80,5 @@ aFTR_PROFILE iam_tool = {
 	0,
 	0,
 	&fTOOL_func,
+    // clang-format on
 };
--- src/ftr/ac_utiwa.c
+++ src/ftr/ac_utiwa.c
@@ -1,17 +1,61 @@
-static void fUW_ct(FTR_ACTOR* ftr_actor, u8* data);
-static void fUW_mv(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data);
-static void fUW_dw(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data);
-static void fUW_dt(FTR_ACTOR* ftr_actor, u8* data);
+static void fUW_ct(FTR_ACTOR* ftr_actor, u8* data) {
+    // nothing
+}
+
+static void fUW_mv(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data) {
+    // nothing
+}
+
+static void fUW_dt(FTR_ACTOR* ftr_actor, u8* data) {
+    // nothing
+}
+
+extern Gfx obj_shop_utiwa1_mat_model[];
+extern Gfx obj_shop_utiwa2_mat_model[];
+extern Gfx obj_shop_utiwa3_mat_model[];
+extern Gfx obj_shop_utiwa4_mat_model[];
+extern Gfx obj_shop_utiwa5_mat_model[];
+extern Gfx obj_shop_utiwa6_mat_model[];
+extern Gfx obj_shop_utiwa7_mat_model[];
+extern Gfx obj_shop_utiwa8_mat_model[];
+
+static Gfx* fUW_mat_table[] = {
+    obj_shop_utiwa1_mat_model, obj_shop_utiwa2_mat_model, obj_shop_utiwa3_mat_model, obj_shop_utiwa4_mat_model,
+    obj_shop_utiwa5_mat_model, obj_shop_utiwa6_mat_model, obj_shop_utiwa7_mat_model, obj_shop_utiwa8_mat_model,
+};
+
+extern Gfx obj_shop_utiwa1_gfx_model[];
+extern Gfx obj_shop_utiwa2_gfx_model[];
+extern Gfx obj_shop_utiwa3_gfx_model[];
+extern Gfx obj_shop_utiwa4_gfx_model[];
+extern Gfx obj_shop_utiwa5_gfx_model[];
+extern Gfx obj_shop_utiwa6_gfx_model[];
+extern Gfx obj_shop_utiwa7_gfx_model[];
+extern Gfx obj_shop_utiwa8_gfx_model[];
+
+static Gfx* fUW_gfx_table[] = {
+    obj_shop_utiwa1_gfx_model, obj_shop_utiwa2_gfx_model, obj_shop_utiwa3_gfx_model, obj_shop_utiwa4_gfx_model,
+    obj_shop_utiwa5_gfx_model, obj_shop_utiwa6_gfx_model, obj_shop_utiwa7_gfx_model, obj_shop_utiwa8_gfx_model,
+};
+
+static void fUW_dw(FTR_ACTOR* ftr_actor, ACTOR* my_room_actor, GAME* game, u8* data) {
+    int utiwa_idx = (ftr_actor->name - 0x453) & 7;
+
+    OPEN_DISP(game->graph);
+
+    gSPMatrix(NEXT_POLY_OPA_DISP, _Matrix_to_Mtx_new(game->graph), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPDisplayList(NEXT_POLY_OPA_DISP, fUW_mat_table[utiwa_idx]);
+    gSPDisplayList(NEXT_POLY_OPA_DISP, fUW_gfx_table[utiwa_idx]);
+
+    CLOSE_DISP(game->graph);
+}
 
 static aFTR_vtable_c fUW_func = {
-	&fUW_ct,
-	&fUW_mv,
-	&fUW_dw,
-	&fUW_dt,
-	NULL,
+    &fUW_ct, &fUW_mv, &fUW_dw, &fUW_dt, NULL,
 };
 
 aFTR_PROFILE iam_utiwa = {
+    // clang-format off
 	NULL,
 	NULL,
 	NULL,
@@ -29,4 +73,5 @@ aFTR_PROFILE iam_utiwa = {
 	0,
 	0,
 	&fUW_func,
+    // clang-format on
 };
