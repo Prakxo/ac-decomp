d7e750a3207941a551112a7d582e97573ac6bb7c
Prakxo <lycos2003@gmail.com>
2024-04-21 17:30:08 +0200

    link ef_ase2

--- src/ef_ase2.c
+++ src/ef_ase2.c
@@ -1,9 +1,19 @@
 #include "ef_effect_control.h"
+#include "m_common_data.h"
 
 static void eAS2_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1);
 static void eAS2_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg);
 static void eAS2_mv(eEC_Effect_c* effect, GAME* game);
 static void eAS2_dw(eEC_Effect_c* effect, GAME* game);
+
+extern Gfx ef_ase02_00_modelT[];
+extern Gfx ef_ase02_01_modelT[];
+extern Gfx ef_ase02_02_modelT[];
+extern Gfx ef_ase02_03_modelT[];
+
+static Gfx* eAS2_model_table[] = {
+    ef_ase02_00_modelT,ef_ase02_01_modelT,ef_ase02_02_modelT,ef_ase02_03_modelT,
+};
 
 eEC_PROFILE_c iam_ef_ase2 = {
     // clang-format off
@@ -17,18 +27,43 @@ eEC_PROFILE_c iam_ef_ase2 = {
     // clang-format on
 };
 
+
 static void eAS2_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1) {
-    // TODO
+    xyz_t offs;
+
+    offs.x = 0.0f;
+    offs.y = 0.0f;
+    offs.z = 30.0f;
+    eEC_CLIP->make_effect_proc(eEC_EFFECT_ASE2,pos,&offs,game,NULL,item_name,prio,0,0);
 }
 
 static void eAS2_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg) {
-    // TODO
+    effect->scale.x = 0.006f;
+    effect->scale.y = 0.006f;    
+    effect->scale.z = 0.006f;
+    effect->effect_specific[0] = 0;
+    effect->timer = 26;
 }
 
 static void eAS2_mv(eEC_Effect_c* effect, GAME* game) {
-    // TODO
+    eEC_CLIP->set_continious_env_proc(effect,26,52);
+    effect->effect_specific[0]++;
+    sAdo_OngenPos((u32) effect, 0x29, &effect->position);
 }
-
 static void eAS2_dw(eEC_Effect_c* effect, GAME* game) {
-    // TODO
+    s16 idx = (s16)((effect->effect_specific[0] >> 2) & 3);
+    xyz_t* scale = &effect->scale;
+
+    OPEN_DISP(game->graph);
+    if (scale) {
+        
+    }
+    else {
+        
+    }
+    
+    eEC_CLIP->auto_matrix_xlu_offset_proc(game, &effect->position, scale, &effect->offset);
+    gSPDisplayList(NEXT_POLY_XLU_DISP, eAS2_model_table[idx]);
+
+    CLOSE_DISP(game->graph);
 }
