dd463a6e6fc3369f75eda06d9599708db67783a3
Prakxo <lycos2003@gmail.com>
2024-06-08 17:52:24 +0200

    link jaudio os

--- include/jaudio_NES/os.h
+++ include/jaudio_NES/os.h
@@ -4,6 +4,14 @@
 #include "types.h"
 #include "libultra/libultra.h"
 
-extern int Z_osSendMesg(OSMesgQueue* mq, OSMesg msg, s32 block);
+extern void Z_osWritebackDCacheAll();
+extern void osInvalDCache2(void* src, s32 size);
+extern void osWritebackDCache2(void* src, s32 size);
+extern void Z_osCreateMesgQueue (OSMesgQueue* mq, OSMesg* msg, s32 count );
+extern s32 Z_osSendMesg(OSMesgQueue* mq, OSMesg msg, s32 flags );
+extern s32 Z_osRecvMesg(OSMesgQueue* mq, OSMesg* msg, s32 flags );
+extern s32 Z_osEPiStartDma (OSPiHandle * handler, OSIoMesg* msg, s32 dir);
+extern void Z_bcopy (void* dst, void* src, size_t size);
+
 
 #endif
