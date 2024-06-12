5d9a146bc3a4466e523b57ad18cf7d5d0f4c61d3
Prakxo <lycos2003@gmail.com>
2024-06-12 17:34:59 +0200

    link dvdthread

--- include/jaudio_NES/dvdthread.h
+++ include/jaudio_NES/dvdthread.h
@@ -5,12 +5,26 @@
 #include "dolphin/dvd.h"
 
 typedef void (*Jac_DVDCallback)(u32);
+typedef s32 (*TaskCallback)(void*);
+typedef void (*ErrorCallback)(char*, u8* addr);
 
-extern s32 DVDT_LoadtoARAM(u32 owner, char* path, u32 dram, u32 aram, u32 len, u32* outLen, Jac_DVDCallback callback);
-extern s32 DVDT_DRAMtoARAM(u32 owner, u32 dram, u32 aram, u32 len, u32* outLen, Jac_DVDCallback callback);
-extern s32 DVDT_ARAMtoDRAM(u32 owner, u32 dram, u32 aram, u32 len, u32* outLen, Jac_DVDCallback callback);
-extern u32 Jac_CheckFile(char* path);
-extern void* jac_dvdproc(void* param);
-extern void jac_dvdproc_init(void);
+extern void DVDT_ExtendPath(char* dst, char* ext);
+extern s32 DVDT_AddTaskHigh(TaskCallback callback, void* stackp, size_t len);
+extern s32 DVDT_AddTask(TaskCallback callback, void* stackp, size_t len);
+extern void jac_dvdproc_init();
+extern void* jac_dvdproc(void*);
+extern s32 DVDT_LoadtoARAM_Main(void* arg);
+extern s32 DVDT_LoadtoARAM(u32 owner, char* name, u32 dst, u32 src, u32 length, u32* status, Jac_DVDCallback callback);
+extern s32 DVDT_ARAMtoDRAM_Main(void* arg);
+extern s32 DVDT_DRAMtoARAM_Main(void* arg);
+extern s32 DVDT_ARAMtoDRAM(u32 owner, u32 dst, u32 src, u32 length, u32* status, Jac_DVDCallback callback);
+extern s32 DVDT_DRAMtoARAM(u32 owner, u32 dst, u32 src, u32 length, u32* status, Jac_DVDCallback callback);
+extern s32 DVDT_CheckFile(char* file);
+extern void DVDT_CheckPass(u32 owner, u32* status, Jac_DVDCallback callback);
+extern s32 Jac_CheckFile(char* file);
+extern void Jac_RegisterDVDErrorCallback(ErrorCallback callback);
+extern s32 Jac_RegisterFastOpen(char* file);
+extern BOOL Jac_DVDOpen(char* name, DVDFileInfo* info);
+
 
 #endif
