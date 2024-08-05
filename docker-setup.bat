014a0af0a1fa8597c3792e1b0a19a91c3b9c75cc
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2024-08-05 05:34:19 +0000

    Rearrange docker command

--- docker-setup.bat
+++ docker-setup.bat
@@ -1,7 +1,6 @@
 @echo off
 echo "Please ensure Docker Desktop is running before continuing."
 pause
-docker build -t ac-decomp .
 curl -O https://files.decomp.dev/compilers_latest.zip
 mkdir compilers_temp
 tar -xf compilers_latest.zip -C compilers_temp
@@ -10,6 +9,7 @@ xcopy compilers_temp\GC\1.3.2\ tools\1.3.2\ /E /I
 xcopy compilers_temp\GC\1.3.2r\ tools\1.3.2r\ /E /I
 del compilers_latest.zip
 rmdir /S /Q compilers_temp\
+docker build -t ac-decomp .
 echo "Docker image setup is now complete. You may proceed with the instructions."
 pause
 exit
