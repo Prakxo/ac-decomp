9db2f8d990dc2930553fdb12ce3000722d50739c
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2024-08-05 04:15:28 +0000

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -8,6 +8,13 @@ else
     exit 0
 fi
 
-mkdir compilers_temp && if [ -e /bin/unzip ]; then unzip compilers_latest.zip -d compilers_temp; else echo "You do not seem to have unzip installed on this system. Please install it to continue."; exit 1; fi
+mkdir compilers_temp
+if [ -e /bin/unzip ]; then
+    unzip compilers_latest.zip -d compilers_temp
+else
+    echo "You do not seem to have unzip installed on this system. Please install it to continue."
+    exit 1
+fi
 mv compilers_temp/GC/1.2.5n/ tools/ && mv compilers_temp/GC/1.3.2/ tools/ && mv compilers_temp/GC/1.3.2r/ tools/
+rm -r compilers_temp compilers_latest.zip
 echo "Docker image setup is now complete. You may proceed with the instructions."
