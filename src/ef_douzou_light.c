5c45af9a3cea1a365a5d7fb1ad6b9e90d813269e
Cuyler36 <24523422+Cuyler36@users.noreply.github.com>
2024-08-06 04:40:29 -0400

    Implement & link ef_douzou_light

--- src/ef_douzou_light.c
+++ src/ef_douzou_light.c
@@ -1,5 +1,9 @@
 #include "ef_effect_control.h"
 
+#include "m_common_data.h"
+#include "m_rcp.h"
+#include "sys_matrix.h"
+
 static void eDouzou_Light_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1);
 static void eDouzou_Light_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg);
 static void eDouzou_Light_mv(eEC_Effect_c* effect, GAME* game);
@@ -18,17 +22,61 @@ eEC_PROFILE_c iam_ef_douzou_light = {
 };
 
 static void eDouzou_Light_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1) {
-    // TODO
+    eEC_CLIP->make_effect_proc(eEC_EFFECT_DOUZOU_LIGHT, pos, NULL, game, NULL, item_name, prio, arg0, arg1);
 }
 
 static void eDouzou_Light_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg) {
-    // TODO
+    switch (effect->arg0) {
+        case 0:
+            effect->offset.x = 0.012f;
+            break;
+        case 1:
+            effect->offset.x = 0.011f;
+            break;
+        case 2:
+            effect->offset.x = 0.010f;
+            break;
+        default:
+            effect->offset.x = 0.009f;
+            break;
+    }
+
+    effect->timer = 24;
 }
 
 static void eDouzou_Light_mv(eEC_Effect_c* effect, GAME* game) {
-    // TODO
+    if (effect->timer > 12) {
+        effect->scale.x = eEC_CLIP->calc_adjust_proc(effect->timer, 12, 24, effect->offset.x, 0.0f);
+    } else {
+        effect->scale.x = eEC_CLIP->calc_adjust_proc(effect->timer, 0, 12, 0.0f, effect->offset.x);
+    }
+
+    effect->scale.y = effect->scale.z = effect->scale.x;
 }
 
+extern Gfx ef_carhosi01_00_modelT[];
+
 static void eDouzou_Light_dw(eEC_Effect_c* effect, GAME* game) {
-    // TODO
+    OPEN_DISP(game->graph);
+
+    eEC_CLIP->auto_matrix_xlu_proc(game, &effect->position, &effect->scale);
+
+    switch (effect->arg0) {
+        case 0:
+            gDPSetEnvColor(NEXT_POLY_XLU_DISP, 255, 255, 0, 255);
+            break;
+        case 1:
+            gDPSetEnvColor(NEXT_POLY_XLU_DISP, 200, 255, 255, 255);
+            break;
+        case 2:
+            gDPSetEnvColor(NEXT_POLY_XLU_DISP, 255, 100, 100, 255);
+            break;
+        default:
+            gDPSetEnvColor(NEXT_POLY_XLU_DISP, 100, 255, 100, 255);
+            break;
+    }
+
+    gSPDisplayList(NEXT_POLY_XLU_DISP, ef_carhosi01_00_modelT);
+
+    CLOSE_DISP(game->graph);
 }
