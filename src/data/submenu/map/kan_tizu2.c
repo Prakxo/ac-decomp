3d30d497220b9ba19657be2eeb30d6c4e2473f78
Cuyler36 <24523422+Cuyler36@users.noreply.github.com>
2024-08-08 01:03:56 -0400

    Add in map icon data, kan_tizu[2]

---
+++ src/data/submenu/map/kan_tizu2.c
@@ -0,0 +1,61 @@
+// clang-format off
+
+extern unsigned char kan_tizu_tst1_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_tst1_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_tr1_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_tr1_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_t_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_t_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fsh_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fsh_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fpo_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fpo_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fpk_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fpk_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fmh_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fmh_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fko_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fko_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_mr1_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_mr1_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_m_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_m_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_mr1b_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_mr1b_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_tr1b_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_tr1b_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fta_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fta_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_mwf_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_mwf_TA_tex_txt.inc"
+};
+
+extern unsigned char kan_tizu_fmu_TA_tex_txt[] __attribute__((aligned(32))) = {
+    #include "assets/submenu/map/kan_tizu_fmu_TA_tex_txt.inc"
+};
