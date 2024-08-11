365e2ec57e15fe985bf1c647593e00f1dd2a3af7
Cuyler36 <24523422+Cuyler36@users.noreply.github.com>
2024-08-11 08:21:27 -0400

    Implement & link all evw_anime files

--- src/data/field/bg/acre/grd_s_r2_b_1/grd_s_r2_b_1_evw_anime.c
+++ src/data/field/bg/acre/grd_s_r2_b_1/grd_s_r2_b_1_evw_anime.c
@@ -2,8 +2,6 @@
 
 static EVW_ANIME_SCROLL grd_s_r2_b_1_evw_anime_1[] = { { 1, -1, 32, 32 }, { -1, -2, 32, 32 } };
 
-extern EVW_ANIME_DATA grd_s_r2_b_1_evw_anime = {
-    -1,
-    EVW_ANIME_TYPE_SCROLL2
-    grd_s_r2_b_1_evw_anime_1
+extern EVW_ANIME_DATA grd_s_r2_b_1_evw_anime[] = {
+    { -1, EVW_ANIME_TYPE_SCROLL2, grd_s_r2_b_1_evw_anime_1 },
 };
--- src/data/field/bg/acre/tmpr2/tmpr2_evw_anime.c
+++ src/data/field/bg/acre/tmpr2/tmpr2_evw_anime.c
@@ -2,8 +2,6 @@
 
 static EVW_ANIME_SCROLL tmpr2_evw_anime_1[] = { { 1, -1, 32, 32 }, { -1, -2, 32, 32 } };
 
-extern EVW_ANIME_DATA tmpr2_evw_anime = {
-    -1,
-    EVW_ANIME_TYPE_SCROLL2
-    tmpr2_evw_anime_1
+extern EVW_ANIME_DATA tmpr2_evw_anime[] = {
+    { -1, EVW_ANIME_TYPE_SCROLL2, tmpr2_evw_anime_1 },
 };
