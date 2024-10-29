b0c1fabcc44e13870377077392510fcbd4b59262
Cuyler36 <24523422+Cuyler36@users.noreply.github.com>
2024-10-28 20:28:46 -0400

    Implement & link ac_npc_p_sel

--- include/ac_npc_p_sel.h
+++ include/ac_npc_p_sel.h
@@ -3,10 +3,27 @@
 
 #include "types.h"
 #include "m_actor.h"
+#include "ac_npc.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct npc_p_sel_s NPC_P_SEL_ACTOR;
+
+typedef void (*aNPS_TALK_PROC)(NPC_P_SEL_ACTOR*, GAME_PLAY*);
+
+/* sizeof(NPC_P_SEL_ACTOR) == 0x9B0 */
+struct npc_p_sel_s {
+    /* 0x000 */ NPC_ACTOR npc_class;
+    /* 0x994 */ int talk_idx;
+    /* 0x998 */ int next_talk_idx;
+    /* 0x99C */ aNPS_TALK_PROC talk_proc;
+    /* 0x9A0 */ int talk_end_flag;
+    /* 0x9A4 */ int silent_counter;
+    /* 0x9A8 */ int strum_timer;
+    /* 0x9AC */ int bgm_stop_timer;
+};
 
 extern ACTOR_PROFILE Npc_P_Sel_Profile;
 
